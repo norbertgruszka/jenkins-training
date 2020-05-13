161659709459d6b1ea5c79b622c390885b9fa912
norbertgruszka <norbert.gruszka@relayr.io>
2020-05-13 16:53:57 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Greeter',
+    version='1.0',
+    description='We all strugell to say hello sometime... ',
+    author='Peter "Frying" Pan',
+    author_email='admin@admin.admin',
+    url='',
+    packages=['greeter'],
+)
